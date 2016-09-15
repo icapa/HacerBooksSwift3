13ebf2ddcfbc6552720d684f6d3a7355d251ab09
icapa <ivan.olea@gmail.com>
2016-09-15 10:08:31 +0200

    Initial Commit

---
+++ HacerBookSwift3Tests/HacerBookSwift3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HacerBookSwift3Tests.swift
+//  HacerBookSwift3Tests
+//
+//  Created by Iván Cayón Palacio on 15/9/16.
+//  Copyright © 2016 KeepCoding. All rights reserved.
+//
+
+import XCTest
+@testable import HacerBookSwift3
+
+class HacerBookSwift3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
